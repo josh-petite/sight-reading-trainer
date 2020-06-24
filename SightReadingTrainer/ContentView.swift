4e052aa9f4edf09d024cc61c4fa93541b9b91cc3
Josh Petite <josh.petite@gmail.com>
2020-06-23 19:25:59 -0700

    Initial Commit

---
+++ SightReadingTrainer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SightReadingTrainer
+//
+//  Created by Josh Petite on 6/23/20.
+//  Copyright © 2020 Josh Petite. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
